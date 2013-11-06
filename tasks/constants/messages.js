d9df92c01146a1fa5e32ac8c12c64759d578473e
Ben Briggs <therealbenbriggs@hotmail.com>
2013-11-06 22:34:03 +0000

    Improved reporter; changed style, outputs expected indentation level.

--- tasks/constants/messages.js
+++ tasks/constants/messages.js
@@ -1,8 +1,8 @@
 module.exports = {
-	INDENTATION_TABS: 'no correct indentation with tabs.',
-	INDENTATION_SPACES: 'no correct indentation with spaces.',
-	INDENTATION_SPACES_AMOUNT: 'incorrect amout of spaces as indentation.',
-	TRAILINGSPACES: 'trailing spaces found.',
-	NEWLINE: 'no newline at end of file.',
-	NEWLINE_AMOUNT: 'too many new lines at end of file.'
+	INDENTATION_TABS          : 'Unexpected spaces found.',
+	INDENTATION_SPACES        : 'Unexpected tabs found.',
+	INDENTATION_SPACES_AMOUNT : 'Expected an indentation at {a} instead of at {b}.',
+	TRAILINGSPACES            : 'Unexpected trailing spaces found.',
+	NEWLINE                   : 'Expected a newline at the end of the file.',
+	NEWLINE_AMOUNT            : 'Unexpected additional newlines at the end of the file.'
 };
