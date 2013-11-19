4254d6d8ce66e84343deaed65a9fae84d1097866
Norman Rusch <norman.rusch@moccu.com>
2013-11-19 11:30:03 +0100

    Change indentation of values
    Previous indentations will be harder to maintain

--- tasks/constants/messages.js
+++ tasks/constants/messages.js
@@ -1,8 +1,8 @@
 module.exports = {
-	INDENTATION_TABS          : 'Unexpected spaces found.',
-	INDENTATION_SPACES        : 'Unexpected tabs found.',
-	INDENTATION_SPACES_AMOUNT : 'Expected an indentation at {a} instead of at {b}.',
-	TRAILINGSPACES            : 'Unexpected trailing spaces found.',
-	NEWLINE                   : 'Expected a newline at the end of the file.',
-	NEWLINE_AMOUNT            : 'Unexpected additional newlines at the end of the file.'
+	INDENTATION_TABS: 'Unexpected spaces found.',
+	INDENTATION_SPACES: 'Unexpected tabs found.',
+	INDENTATION_SPACES_AMOUNT: 'Expected an indentation at {a} instead of at {b}.',
+	TRAILINGSPACES: 'Unexpected trailing spaces found.',
+	NEWLINE: 'Expected a newline at the end of the file.',
+	NEWLINE_AMOUNT: 'Unexpected additional newlines at the end of the file.'
 };
