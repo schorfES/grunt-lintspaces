705f6b141cee47d3f560a4158d2709cfb7b9498a
Norman Rusch <norman.rusch@moccu.com>
2014-02-12 10:40:18 +0100

    Refactored prototypical extension

--- tasks/lintspaces/Logger.js
+++ tasks/lintspaces/Logger.js
@@ -1,5 +1,6 @@
 var
-	MESSAGES = require('./constants/messages')
+	MESSAGES = require('./constants/messages'),
+	merge = require('merge')
 ;
 
 module.exports = (function() {
@@ -8,52 +9,55 @@ module.exports = (function() {
 		this._messages = {};
 	};
 
-	Logger.prototype._add = function(path, message) {
-		if (typeof this._messages[path] !== 'object') {
-			this._messages[path] = [];
-		}
+	// Extend Logger's prototype with specific properties:
+	Logger.prototype = merge(Logger.prototype, {
+		_add: function(path, message) {
+			if (typeof this._messages[path] !== 'object') {
+				this._messages[path] = [];
+			}
 
-		this._messages[path].push(message);
-	};
+			this._messages[path].push(message);
+		},
 
-	Logger.prototype.setAmountOfProcessedFiles = function(amount) {
-		this._amountOfFiles = amount;
-	};
+		setAmountOfProcessedFiles: function(amount) {
+			this._amountOfFiles = amount;
+		},
 
-	Logger.prototype.logLine = function(path, linenumber, message) {
-		this._add(path, 'L' + linenumber + ': ' + message.yellow);
-	};
+		logLine: function(path, linenumber, message) {
+			this._add(path, 'L' + linenumber + ': ' + message.yellow);
+		},
 
-	Logger.prototype.fail = function(message) {
-		this._grunt.fail.warn(message);
-	};
+		fail: function(message) {
+			this._grunt.fail.warn(message);
+		},
 
-	Logger.prototype.flush = function() {
-		var
-			self = this,
-			out = ''
-		;
+		flush: function() {
+			var
+				self = this,
+				out = ''
+			;
 
-		Object.keys(this._messages).forEach(function(path) {
-			out += 'ERROR: '.red + path.underline + '\n';
+			Object.keys(this._messages).forEach(function(path) {
+				out += 'ERROR: '.red + path.underline + '\n';
 
-			self._messages[path].forEach(function(message) {
-				out += message + '\n';
+				self._messages[path].forEach(function(message) {
+					out += message + '\n';
+				});
 			});
-		});
 
-		if (out.length > 0) {
-			this._grunt.log.writeln(out);
-			this.fail(MESSAGES.FAILED_LINTING);
-		} else {
-			this._grunt.log.ok(
-				MESSAGES.PASSED_LINTING.replace(
-					'{a}',
-					this._amountOfFiles + ' file' + ((this._amountOfFiles > 1) ? 's' : '')
-				)
-			);
+			if (out.length > 0) {
+				this._grunt.log.writeln(out);
+				this.fail(MESSAGES.FAILED_LINTING);
+			} else {
+				this._grunt.log.ok(
+					MESSAGES.PASSED_LINTING.replace(
+						'{a}',
+						this._amountOfFiles + ' file' + ((this._amountOfFiles > 1) ? 's' : '')
+					)
+				);
+			}
 		}
-	};
+	});
 
 	return Logger;
 })();
